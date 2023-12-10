7156b5d6b67dad3fd2644011982b231a698dc9a6
Tom KAUFFELD <tom.kauffeld@gmail.com>
2023-12-10 15:13:49 +0100

    announced: day 10

---
+++ day10/part1/main.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'utils' . DIRECTORY_SEPARATOR . 'utils.php';
+
+$file = getInputFile(10);
+
+fclose($file);
---
+++ day10/part2/README.md
@@ -0,0 +1,2 @@
+# [Advent of Code 2023](../../README.md) - [Day 10](../README.md) - Part 2
+
