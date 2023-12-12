fe8ca22109485c85d136c99f5d878ade6e57592f
Tom KAUFFELD <tom.kauffeld@gmail.com>
2023-12-12 17:52:23 +0100

    solved: day 12 - part 2

--- day12/part2/main.php
+++ day12/part2/main.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'utils' . DIRECTORY_SEPARATOR . 'utils.php';
 
-//$file = getInputFile(12);
-$file = fopen('test.txt', 'r');
+$file = getInputFile(12);
+//$file = fopen('test.txt', 'r');
 
 $sum = 0;
 while (($line = fgets($file)) !== false)
@@ -15,70 +15,25 @@ while (($line = fgets($file)) !== false)
     $springs = "$springs?$springs?$springs?$springs?$springs";
     $damaged = getNumbers($parts[1], ',');
     $damaged = array_merge($damaged, $damaged, $damaged, $damaged, $damaged);
-    //$possibles = getPossibles($springs, null, ...$damaged);
-    //$cPossibles = count($possibles);
     $cPossibles = getPossiblesCount($springs, null, ...$damaged);
     $sum += $cPossibles;
     print ("$cPossibles\t - $sum\n");
 }
 
-
-fclose($file);
-
-
-
-function getPossibles(string $springs, ?int $remaining = null, int ... $damaged): array
-{
-    $d = array_shift($damaged);
-    if ($d === null || $d < 1) {
-        $index = stripos($springs, '#');
-        if ($index !== false)
-            return [];
-        return [str_replace('?', '.', $springs)];
-    }
-
-    if ($remaining === null)
-    {
-        $remaining = 0;
-        foreach ($damaged as $i)
-            $remaining += $i;
-    } else {
-        $remaining -= $d;
-    }
-
-    $sSprings = strlen($springs);
-    $possibles = [];
-    $iMax = $sSprings - $d - $remaining + 1;
-    for ($i = 0; $i < $iMax; ++$i)
-    {
-        $ok = $i + $d === $sSprings || $springs[$i + $d] === '.' || $springs[$i + $d] === '?';
-        $ok &= $i === 0 || $springs[$i - 1] === '.' || $springs[$i - 1] === '?';
-
-        for ($j = $i; $j < $sSprings && $j < $i + $d && $ok; ++$j)
-        {
-            if ($springs[$j] !== '?' && $springs[$j] !== '#')
-                $ok = false;
-        }
-        if ($ok)
-        {
-            if ($i + $d === $sSprings) {
-                return [createPrefix($springs, $i, $d)];
-            } else {
-                $prefix = createPrefix($springs, $i, $d);
-                $subSprings = substr($springs, $i + $d + 1);
-                $subPossibles = getPossibles($subSprings, $remaining, ... $damaged);
-                foreach ($subPossibles as $item)
-                {
-                    $possibles[] = "$prefix.$item";
-                }
-            }
-        }
-    }
-    return $possibles;
-}
+global $cacheHits;
+global $cache;
+$cache = [];
+$cacheHits = 0;
 
 function getPossiblesCount(string $springs, ?int $remaining = null, int ... $damaged): int
 {
+    global $cache;
+    $key = $springs.'-'.join('-', $damaged);
+    if (isset($cache[$key])) {
+        global $cacheHits;
+        ++$cacheHits;
+        return $cache[$key];
+    }
     $d = array_shift($damaged);
     if ($d === null || $d < 1) {
         $index = stripos($springs, '#');
@@ -101,18 +56,21 @@ function getPossiblesCount(string $springs, ?int $remaining = null, int ... $dam
     $iMax = $sSprings - $d - $remaining + 1;
     for ($i = 0; $i < $iMax; ++$i)
     {
+        if ($springs[$i] === '#')
+            $iMax = min($i + 1, $iMax);
         $ok = $i + $d === $sSprings || $springs[$i + $d] === '.' || $springs[$i + $d] === '?';
         $ok &= $i === 0 || $springs[$i - 1] === '.' || $springs[$i - 1] === '?';
 
         for ($j = $i; $j < $sSprings && $j < $i + $d && $ok; ++$j)
         {
+
             if ($springs[$j] !== '?' && $springs[$j] !== '#')
                 $ok = false;
         }
         if ($ok)
         {
             if ($i + $d === $sSprings) {
-                return 1;
+                $possibles += 1;
             } else {
                 $subSprings = substr($springs, $i + $d + 1);
                 $subPossibles = getPossiblesCount($subSprings, $remaining, ... $damaged);
@@ -120,6 +78,7 @@ function getPossiblesCount(string $springs, ?int $remaining = null, int ... $dam
             }
         }
     }
+    $cache[$key] = $possibles;
     return $possibles;
 }
 
