58b1e21e08370bb21f9bed617d9d48c0e05ef685
Tom KAUFFELD <tom.kauffeld@gmail.com>
2023-12-14 16:22:41 +0100

    solved: day 14 - part 1

--- day14/input.php
+++ day14/input.php
@@ -14,9 +14,40 @@ function getInput($day): array
     else
         throw new InvalidArgumentException('day must be a filename or a day number');
 
+    $field = [];
+    $width = null;
+
+    $caseMap = [
+        '.' => 0,
+        'O' => 1,
+        '#' => 2,
+    ];
+
+    while(($line = fgets($file)) !== false)
+    {
+        $line = trim($line);
+        $sLine = strlen($line);
+        if ($sLine < 1)
+            continue;
+        if ($width !== null && $sLine !== $width)
+            throw new RuntimeException('invalid map width');
+        if ($width === null)
+            $width = $sLine;
+
+        $row = [];
+
+        for ($x = 0; $x < $width; ++$x)
+        {
+            if (!isset($caseMap[$line[$x]]))
+                throw new RuntimeException('invalid char');
+            else
+                $row[$x] = $caseMap[$line[$x]];
+        }
+        $field[] = $row;
+    }
 
 
     fclose($file);
 
-    return [];
+    return $field;
 }
--- day14/part1/main.php
+++ day14/part1/main.php
@@ -2,4 +2,28 @@
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'input.php';
 
 
-$data = getInput(14);
+$field = getInput(14);
+
+$h = count($field);
+$w = count($field[0]);
+$sum = 0;
+$offsets = array_fill(0, $w, 0);
+
+for ($y = 0; $y < $h; ++$y)
+{
+    $per = round($y * 100 / $h);
+    print("$y / $h -> $per%\n");
+    for ($x = 0; $x < $w; ++$x)
+    {
+        if ($field[$y][$x] === 2)
+            $offsets[$x] = $y + 1;
+        elseif ($field[$y][$x] === 1)
+        {
+            $weight = $h - $offsets[$x];
+            ++$offsets[$x];
+            $sum += $weight;
+        }
+    }
+}
+
+print ("Total weight: $sum\n");
