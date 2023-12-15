ca022f76fd2848b9e6be52f50bf4d84539ef38b7
Tom KAUFFELD <tom.kauffeld@gmail.com>
2023-12-15 10:18:01 +0100

    announced: day 15

---
+++ day15/part1/README.md
@@ -0,0 +1,2 @@
+# [Advent of Code 2023](../../README.md) - [Day 15](../README.md) - Part 1
+
---
+++ day15/part1/main.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'utils' . DIRECTORY_SEPARATOR . 'utils.php';
+
+
+$file = getInputFile(15);
+
+
+
+fclose($file);
